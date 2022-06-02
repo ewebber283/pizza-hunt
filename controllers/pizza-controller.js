5efd0f1d5d5be0eff9338adae415bd72e13e6772
Eric Webber <ewebber283@gmail.com>
2022-06-01 19:41:59 -0500

    created user model and added crud routes for pizza

---
+++ controllers/pizza-controller.js
@@ -0,0 +1,62 @@
+const { Pizza } = require('../models');
+
+const pizzaController = {
+    //get all
+    getAllPizza(req,res) {
+        Pizza.find({})
+            .then(dbPizzaData => res.json(dbPizzaData))
+            .catch(err => {
+                console.log(err);
+                res.status(400).json(err);
+            });
+    },
+    //get one
+    getPizzaById({ params }, res) {
+        Pizza.find({_id: params.id })
+        .then(dbPizzaData => {
+        if(!dbPizzaData) {
+            res.status(400).json({ message: 'No pizza found with this id'});
+            return;
+        }
+            res.json(dbPizzaData);
+        })
+            .catch(err => {
+                console.log(err);
+                res.status(400).json(err);
+            });
+    },
+    createPizza({ body }, res) {
+        Pizza.create(body)
+        .then(dbPizzaData => res.json(dbPizzaData))
+        .catch(err => res.status(400).json(err));
+    },
+    updatePizza({ params }, res) {
+        Pizza.findOneAndUpdate({_id: params.id }, body, { new: true})
+        .then(dbPizzaData => {
+        if(!dbPizzaData) {
+            res.status(400).json({ message: 'No pizza found with this id'});
+            return;
+        }
+            res.json(dbPizzaData);
+        })
+            .catch(err => {
+                res.status(400).json(err);
+            });
+    },
+    deletePizza({ params }, res) {
+        Pizza.findOneAndDelete({_id: params.id })
+        .then(dbPizzaData => {
+        if(!dbPizzaData) {
+            res.status(400).json({ message: 'No pizza found with this id'});
+            return;
+        }
+            res.json(dbPizzaData);
+        })
+            .catch(err => {
+                res.status(400).json(err);
+            });
+    },
+
+};
+
+module.exports = pizzaController;
