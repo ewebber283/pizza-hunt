ffbaf125ab2cb19851f63e261672f1bab2cf1a5d
Eric Webber <ewebber283@gmail.com>
2022-06-04 12:59:05 -0500

    added reply function, indexeddb, and validators

--- controllers/comment-controller.js
+++ controllers/comment-controller.js
@@ -1,28 +1,43 @@
 const { Comment, Pizza } = require('../models');
 
 const commentController = {
-  
-  addComment({ params, body}, res) {
+  // add comment to pizza
+  addComment({ params, body }, res) {
     console.log(body);
     Comment.create(body)
-        .then(({ _id }) => {
-            return Pizza.findOneAndUpdate(
-                {_id: params.pizzaId },
-                { $push: { comments: _id }},
-                { new: true}
-            );
-        })
-        .then(dbPizzaData => {
-            if(!dbPizzaData) {
-                res.status(404).json({ message: 'No pizza with this id'});
-                return;
-            }
-            res.json(dbPizzaData)
-        })
-        .catch(err=>res.json(err))
+      .then(({ _id }) => {
+        return Pizza.findOneAndUpdate(
+          { _id: params.pizzaId },
+          { $push: { comments: _id } },
+          { new: true }
+        );
+      })
+      .then(dbPizzaData => {
+        if (!dbPizzaData) {
+          res.status(404).json({ message: 'No pizza found with this id!' });
+          return;
+        }
+        res.json(dbPizzaData);
+      })
+      .catch(err => res.json(err));
+  },
+  addReply({ params, body }, res) {
+    Comment.findOneAndUpdate(
+      { _id: params.commentId },
+      { $push: { replies: body } },
+      { new: true, runValidators: true }
+    )
+      .then(dbPizzaData => {
+        if (!dbPizzaData) {
+          res.status(404).json({ message: 'No pizza found with this id!' });
+          return;
+        }
+        res.json(dbPizzaData);
+      })
+      .catch(err => res.json(err));
   },
 
-  
+  // remove comment
   removeComment({ params }, res) {
     Comment.findOneAndDelete({ _id: params.commentId })
       .then(deletedComment => {
@@ -43,6 +58,17 @@ const commentController = {
         res.json(dbPizzaData);
       })
       .catch(err => res.json(err));
-  }
+  },
+  // remove reply
+removeReply({ params }, res) {
+  Comment.findOneAndUpdate(
+    { _id: params.commentId },
+    { $pull: { replies: { replyId: params.replyId } } },
+    { new: true }
+  )
+    .then(dbPizzaData => res.json(dbPizzaData))
+    .catch(err => res.json(err));
 }
+};
+
 module.exports = commentController;
--- models/Pizza.js
+++ models/Pizza.js
@@ -3,20 +3,26 @@ const dateFormat = require('../utils/dateFormat');
 
 const PizzaSchema = new Schema({
     pizzaName: {
-        type: String
-    },
-    createdBy: {
-        type: String
-    },
+        type: String,
+        required: true,
+        trim: true
+      },
+      createdBy: {
+        type: String,
+        required: true,
+        trim: true
+      },
     createdAt: {
         type: Date,
         default: Date.now,
-        get: (createdAtVal) => dataFormat(createdAtVal)
+        get: (createdAtVal) => dateFormat(createdAtVal)
     },
-    size:{
+    size: {
         type: String,
+        required: true,
+        enum: ['Personal', 'Small', 'Medium', 'Large', 'Extra Large'],
         default: 'Large'
-    },
+      },
     toppings: [],
     comments: [
         {
@@ -32,10 +38,10 @@ const PizzaSchema = new Schema({
     },
     id: false
 });
-
+//updated to include replies
 PizzaSchema.virtual('commentCount').get(function() {
-    return this.comments.length;
-})
+    return this.comments.reduce((total, comment) => total + comment.replies.length + 1, 0);
+  });
 
 const Pizza = model('Pizza', PizzaSchema);
 
